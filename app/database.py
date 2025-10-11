aa48d6e8a5180e11574391e51a52adc98903ef6a
Roman <romansaushkin99@gmail.com>
2025-10-12 03:59:17 +0500

    Initial commit: Smart Todo App with FastAPI and PostgreSQL

---
+++ app/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy.ext.asyncio import *
+from sqlalchemy.orm import *
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+Database_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(Database_URL, echo = True)
+
+AsyncSessionLocal = sessionmaker(
+    bind = engine,
+    class_ = AsyncSession,
+    expire_on_commit=False
+    )
+
+Base = declarative_base()
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
+
+
+
---
+++ app/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import *
+from sqlalchemy.sql import func
+from database import Base
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index = True, Nullable = False)
+    description = Column(String)
+    create_at = Column(DateTime(timezone=True), server_default=func.now())
+    tags = Column(JSON, nullable=True)
+    meta = Column(JSON, nullable=True)
+
+
+
+
